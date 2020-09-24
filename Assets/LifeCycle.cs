de08439dea013beba6ba24661003f16f8fc3b492
David <vapor2008@naver.com>
2020-09-24 21:53:54 +0900

    RD IDE git push

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -4,43 +4,12 @@ using UnityEngine;
 
 public class LifeCycle : MonoBehaviour
 {
-    void Awake()
-    {
-        Debug.Log("플레이어 데이터가 준비되었습니다.");
-    }
-
-    void onEnable()
-    {
-        Debug.Log("플레이어가 로그인 하였습니다.");
-    }
-
-    void Start()
-    {
-        Debug.Log("사냥 장비를 챙겼습니다.");
-    }
-
-    void FixedUpdate()
-    {
-        Debug.Log("이동");
-    }
-
-    void Update()
-    {
-        Debug.Log("몬스터 사냥");
-    }
-
-    void LateUpdate()
-    {
-        Debug.Log("경험치 획득.");
-    }
-
-    void onDisable()
-    {
-        Debug.Log("플레이어가 로그아웃하였습니다.");
-    }
-
-    void onDestroy()
-    {
-        Debug.Log("플레이어 데이터를 해제하였습니다.");
-    }
+   void Update()
+   {
+      if(Input.anyKeyDown)
+         Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+      
+      if(Input.anyKey)
+         Debug.Log("플레이어가 아무 키를 누르고 있습니다.");
+   }
 }
