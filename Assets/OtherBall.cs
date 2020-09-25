84661a594a56fcf69db1c4b3eb6d4824ce66f86b
David <vapor2008@naver.com>
2020-09-25 22:19:07 +0900

    09.25E11

---
+++ Assets/OtherBall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherBall : MonoBehaviour
+{
+    private MeshRenderer mesh;
+    private Material mat;
+    
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Ball")
+            mat.color = new Color(0,0,0);
+    }
+    
+    void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.name == "Ball")
+            mat.color = new Color(1,1,1);
+    }
+
+}
