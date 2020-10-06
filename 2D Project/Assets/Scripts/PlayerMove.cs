e01aae7d31bd4647bfd94a520128463aec52d90d
David <vapor2008@naver.com>
2020-10-06 21:49:22 +0900

    B19

--- 2D Project/Assets/Scripts/PlayerMove.cs
+++ 2D Project/Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float maxSpeed;
-    public float jumpPower;
+    public float maxSpeed = 4f;
+    public float jumpPower = 15f;
     
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
@@ -55,5 +56,31 @@ public class PlayerMove : MonoBehaviour
                 if (rayHit.distance < 0.5f)
                     anim.SetBool("isJumping", false);
         }
-    } 
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+            onDamaged(other.transform.position);
+    }
+
+    void onDamaged(Vector2 targetPos)
+    {
+        gameObject.layer = 11;
+
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        int direction = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(direction,1) * 10, ForceMode2D.Impulse);
+
+        anim.SetTrigger("doDamaged");
+        
+        Invoke("offDamaged",2);
+    }
+
+    void offDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
 }
